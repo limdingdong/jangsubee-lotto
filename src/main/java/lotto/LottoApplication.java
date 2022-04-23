f1ae021f6fe943afa58c8503f8e52daf3cb1b434
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-04-23 09:28:50 +0900

    Lotto Merge Test

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -2,6 +2,6 @@ package lotto;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        System.out.println("Hello Lotto");
+        System.out.println("Hello Lotto Merge Test");
     }
 }
