9ed5c00d1e58a9d4403230f3a6a3376714637ffe
HeeSeok Noh <nhs0912@nate.com>
2022-04-23 13:20:03 +0900

    test : emtyOrNull test 성공

--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -1,9 +1,13 @@
 package study;
 
+import org.junit.platform.commons.util.StringUtils;
+
 public class StringCalculator {
 
     public int add(String text) {
-
+        if(StringUtils.isBlank(text)){
+            return 0;
+        }
         return 0;
     }
 }
