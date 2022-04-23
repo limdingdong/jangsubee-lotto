1ce249506637c326ea886d15313ee8084d7e41d2
jiokim <jiokim222@gmail.com>
2022-04-23 09:58:42 +0900

    Lotto Message v2

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -2,6 +2,6 @@ package lotto;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        System.out.println("오늘의 로또 주인공은 나!");
+        System.out.println("오늘의 로또 주인공은 너!");
     }
 }
