d29c3ef63d751e319384b8c95b3cbc46c9557842
HeeSeok Noh <nhs0912@nate.com>
2022-04-23 13:30:39 +0900

    test : twoNumbers test 성공

--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -2,16 +2,25 @@ package study;
 
 import org.junit.platform.commons.util.StringUtils;
 
+import java.util.StringTokenizer;
+
 public class StringCalculator {
 
     public int add(String text) {
-        if(StringUtils.isBlank(text)){
+        int sum = 0;
+        if (StringUtils.isBlank(text)) {
             return 0;
         }
 
-        if(text.length() == 1){
+        if (text.length() == 1) {
             return Integer.parseInt(text);
         }
-        return 0;
+
+        StringTokenizer st = new StringTokenizer(text, ",");
+        while (st.hasMoreTokens()) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        return sum;
     }
 }
