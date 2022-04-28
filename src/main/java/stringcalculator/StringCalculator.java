82ad430093efa25aefe2221673c9095c69c210d0
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-04-28 12:51:23 +0900

    feat) 요구사항 구현 - 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,16 +1,31 @@
 package stringcalculator;
 
-public class StringCalculator {
+import java.util.regex.Pattern;
 
+public class StringCalculator {
     private static final int MIN_NUMBER = 0;
+    private static final int SINGLE_INPUT_LENGTH = 1;
+    private static final Pattern NUMERIC_PATTERN = Pattern.compile("[+-]?\\d*(\\.\\d+)?");
+
     public int add(String text) {
         if (isNullOrEmpty(text)) {
             return MIN_NUMBER;
         }
+        if (isSingleInputInteger(text)) {
+            return Integer.parseInt(text);
+        }
         return 0;
     }
-
     private boolean isNullOrEmpty(String text) {
         return text == null || text.isEmpty();
+    }
+    private boolean isSingleInputInteger(String text) {
+        if (text.length() != SINGLE_INPUT_LENGTH) {
+            return false;
+        }
+        return isInteger(text);
+    }
+    private boolean isInteger(String text) {
+        return NUMERIC_PATTERN.matcher(text).matches();
     }
 }
