4022838410e6676923b2dcfd210c3aa17959a3aa
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-05-06 12:24:48 +0900

    feat) 요구사항 구현 - 숫자 두개를 쉼표 구분자로 입력할 경우 두 숫자의 합을 반환한다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,24 +1,29 @@
 package stringcalculator;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final int MIN_NUMBER = 0;
     private static final int SINGLE_INPUT_LENGTH = 1;
     private static final Pattern NUMERIC_PATTERN = Pattern.compile("[+-]?\\d*(\\.\\d+)?");
+    private static final String TOKEN_DELIMITER = ",";
 
-    public int add(String text) {
+    public int add(final String text) {
         if (isNullOrEmpty(text)) {
             return MIN_NUMBER;
         }
         if (isSingleInputInteger(text)) {
             return Integer.parseInt(text);
         }
-        return 0;
+
+        return sum(text.split(TOKEN_DELIMITER));
     }
+
     private boolean isNullOrEmpty(String text) {
         return text == null || text.isEmpty();
     }
+
     private boolean isSingleInputInteger(String text) {
         if (text.length() != SINGLE_INPUT_LENGTH) {
             return false;
@@ -27,5 +32,10 @@ public class StringCalculator {
     }
     private boolean isInteger(String text) {
         return NUMERIC_PATTERN.matcher(text).matches();
+    }
+    private int sum(String[] tokens) {
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
