fa5f7c57eab4916a7e70853c6df9bd0b1a1cdccf
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-05-06 12:51:30 +0900

    refactor : StringCalculator 를 상태를 갖지않는 static util 클래스로 변경

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -14,7 +14,9 @@ public class StringCalculator {
     private static final int CUSTOM_DELIMITER_INDEX = 1;
     private static final int CUSTOM_DELIMITER_INPUT_INDEX = 2;
 
-    public int add(final String text) {
+    private StringCalculator() {}
+
+    public static int add(final String text) {
         if (isNullOrEmpty(text)) {
             return MIN_NUMBER;
         }
@@ -24,19 +26,19 @@ public class StringCalculator {
         return sum(split(text));
     }
 
-    private boolean isNullOrEmpty(String text) {
+    private static boolean isNullOrEmpty(String text) {
         return text == null || text.isEmpty();
     }
-    private boolean isSingleInputInteger(String text) {
+    private static boolean isSingleInputInteger(String text) {
         if (text.length() != SINGLE_INPUT_LENGTH) {
             return false;
         }
         return isInteger(text);
     }
-    private boolean isInteger(String text) {
+    private static boolean isInteger(String text) {
         return NUMERIC_PATTERN.matcher(text).matches();
     }
-    private String[] split(String text) {
+    private static String[] split(String text) {
         Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if (matcher.find()) {
             return splitByCustomDelimiter(matcher);
@@ -44,12 +46,12 @@ public class StringCalculator {
         return text.split(TOKEN_DELIMITER);
     }
 
-    private String[] splitByCustomDelimiter(Matcher matcher) {
+    private static String[] splitByCustomDelimiter(Matcher matcher) {
         String customDelimiter = matcher.group(CUSTOM_DELIMITER_INDEX);
         return matcher.group(CUSTOM_DELIMITER_INPUT_INDEX).split(customDelimiter);
     }
 
-    private int sum(String[] tokens) {
+    private static int sum(String[] tokens) {
         return Arrays.stream(tokens)
                 .mapToInt(UnsignedInteger::parseInt)
                 .sum();
